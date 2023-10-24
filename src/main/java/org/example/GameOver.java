92dd61cbb694fda234d4e51e08c89bccf472c113
fabricioguidine <fabricioguidine@gmail.com>
2023-10-24 17:51:12 -0300

    implementa classe gameover

--- src/main/java/org/example/GameOver.java
+++ src/main/java/org/example/GameOver.java
@@ -1,4 +1,26 @@
 package org.example;
-
-public class GameOver {
+public class GameOver implements JogoEstado{
+    private GameOver() {};
+    private static GameOver instance = new GameOver();
+    public static GameOver getInstance() {
+        return instance;
+    }
+    public String getEstado() {
+        return "GAME OVER!";
+    }
+    public String comecar(Jogo jogo) {
+        return "Jogo não iniciado.";
+    }
+    public String jogar(Jogo jogo) {
+        return "Jogo em andamendo não realizado.";
+    }
+    public String perder(Jogo jogo) {
+        return "Game Over não realizado.";
+    }
+    public String ganhar(Jogo jogo) {
+        return "Vitoria não realizada.";
+    }
+    public String pausar(Jogo jogo) {
+        return "Jogo pausado não realizado.";
+    }
 }
